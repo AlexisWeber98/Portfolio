61b8f3a67092a209109895055eb26d81182addf3
Alexis Weber <alexis.weber@mailfence.com>
2023-10-25 21:25:57 -0300

    delete usless files

--- components/MovileProyects/MovileProyects.tsx
+++
@@ -1,8 +0,0 @@
-export default function MovileProyects() {
-  return (
-    <main>
-      <h3>Barber App</h3>
-      <h3>StickerSmash pro version</h3>
-    </main>
-  );
-}
--- components/Tecnologies/Tecnologies.tsx
+++
@@ -1,9 +0,0 @@
-export default function Tecnologies () {
-    return (
-        <main>
-            <h3>FrontEnd</h3>
-            <div></div>
-            <h3>BackEnd</h3>
-        </main>
-    )
-}
