f5956a04b939adf5c14c5c3b1ba4fb890885659b
Alexis Weber <alexis.weber@mailfence.com>
2023-10-26 23:07:14 -0300

    change photo

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Ale from "../../assets/Ale.png";
+import styles from "./About.module.css";
 export default function About() {
   return (
     <main>
-      <div>
-        {/* <Image
-          width="50"
-          height="50"
+      <div className={styles.photo}>
+        <Image
+          src={Ale}
           alt="Alexis Weber"
-          src="../../assets/Ale.jpg"
-        /> */}
+          width={300}
+          height={300}
+          style={{ borderRadius: "10px" }}
+        />
       </div>
       <h2>About me</h2>
     </main>
