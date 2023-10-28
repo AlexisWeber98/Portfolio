f736fb5ca4c31ba7eee003892bdff7a5d471cd82
Alexis Weber <alexis.weber@mailfence.com>
2023-10-28 13:09:23 -0300

    add metadata and lang

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Alexis Weber",
-  description: "Full Stack Developer",
+  description:
+    "Full Stack Developer (frontend and backend developer) with experience on multiples tecnologies as like: Javascript, Typescript, React, Vite, Next.js, Node.js, Sequelize, Express.js, HTML, Css, Agile, SCRUM, React Native  ",
 };
 
 export default function RootLayout({
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={inter.className}>
         <div className="navContain">
           <NavBar />
