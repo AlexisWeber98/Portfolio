e725a4e629ddc988271992f90fefc7637aac5823
Alexis Weber <alexis.weber@mailfence.com>
2023-11-01 21:57:38 -0300

    add web proyects data

--- app/components/Proyects/Proyects.tsx
+++ app/components/Proyects/Proyects.tsx
@@ -1,7 +1,27 @@
 import styles from "./Proyects.module.css";
+import Cards from "../Cards/Card";
 
 const Proyects = () => {
-  
+  const craftBeer = {
+    name: "CraftBeer",
+    image:
+      "https://craftbeer-team.netlify.app/assets/craftBeerIcon-dbc1f5c6.ico",
+    url: "https://craftbeer-team.netlify.app/",
+  };
+
+  const worldOrfDogs = {
+    name: "WorldOfDogs",
+    image: "",
+    url: "https://world-of-dogs.netlify.app/",
+  };
+
+  const webProyects = [craftBeer, worldOrfDogs];
+  const movileProyects = [];
+
+  const webMap = webProyects.map((proyect) => {
+    return 
+  });
+
   return (
     <main className={styles.container}>
       <h2 className={styles.center}>Proyectos</h2>
@@ -11,12 +31,10 @@ const Proyects = () => {
         <div className={styles.cards}>
           <h3 className="title">Web</h3>
           <hr />
-          
         </div>
         <div className={styles.cards}>
           <h3 className="title">Movil</h3>
           <hr />
-        
         </div>
       </div>
     </main>
